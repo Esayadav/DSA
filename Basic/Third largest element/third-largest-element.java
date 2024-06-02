87e5e63f7f86927d18fa701d4329a2ce12d0c929
Esayadav <147519735+Esayadav@users.noreply.github.com>
2024-06-02 20:41:25 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Third largest element/third-largest-element.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+class ThirdLargestElement
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n =sc.nextInt();
+			int arr[] = new int[n];
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+			Solution g = new Solution();
+			System.out.println(g.thirdLargest(arr,n));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    int thirdLargest(int a[], int n)
+    {
+	    // Your code here
+	      int count=0;
+        int ans = -1;
+        Arrays.sort(a);
+        for(int i=n-1;i>0;i--){
+            count++;
+            if(count==3){
+                ans=a[i];
+                break;
+            }
+            
+        }
+        return ans;
+    }
+}
