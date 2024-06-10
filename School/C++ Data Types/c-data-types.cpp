ec1c8448e4e1a6e78945bb9852a7174012fc3db2
Esayadav <147519735+Esayadav@users.noreply.github.com>
2024-06-10 17:45:07 +0530

    Added Solution - GfG to GitHub

---
+++ School/C++ Data Types/c-data-types.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int cppIntType() {
+        // code here
+        int x; cin >> x; return x;
+    }
+    
+    char cppCharType() {
+        // code here
+        char x; cin >> x; return x;
+    }
+    
+    float cppFloatType() {
+        // code here
+        float x; cin >> x; return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        Solution ob;
+        cout << ob.cppIntType() << endl;
+        cout << ob.cppCharType() << endl;
+        cout << ob.cppFloatType() << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
