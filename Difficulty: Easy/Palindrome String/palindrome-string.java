18b66ccf529aefc52c5f51ecf7e06e1415bc3d59
Esayadav <147519735+Esayadav@users.noreply.github.com>
2025-01-01 15:16:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.java
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -1,39 +1,39 @@
 //{ Driver Code Starts
-//Initial Template for Java
-
 import java.io.*;
-import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
-            String S = read.readLine();
-            
+            String s = read.readLine(); // Use lowercase 's'
+
             Solution ob = new Solution();
-            System.out.println(ob.isPalindrome(S));
+            if (ob.isPalindrome(s)) // Check palindrome
+                System.out.println("true");
+            else
+                System.out.println("false");
+            System.out.println("~");
         }
     }
 }
+
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 class Solution {
-    int isPalindrome(String S) {
+    boolean isPalindrome(String s) {
         // code here
-          StringBuffer str = new StringBuffer(S);
-        str.reverse();
-    
-        for(int i=0;i<S.length();i++){  
-            
-            if(str.charAt(i)!=S.charAt(i))
-               return 0;
-        
+         for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)!=s.charAt(s.length()-1-i))
+            {
+                return false;
+            }
         }
-        return 1;
+        
+        return true;
     }
 };
