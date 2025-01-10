72805a34d0e4d2a1b6aa43fb7a22295e8a879409
Esayadav <147519735+Esayadav@users.noreply.github.com>
2025-01-10 22:34:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+
+            String[] line1 = br.readLine().trim().split(" ");
+            int[] a = new int[line1.length];
+            for (int i = 0; i < line1.length; i++) {
+                a[i] = Integer.parseInt(line1[i]);
+            }
+
+            String[] line2 = br.readLine().trim().split(" ");
+            int[] b = new int[line2.length];
+            for (int i = 0; i < line2.length; i++) {
+                b[i] = Integer.parseInt(line2[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.kthElement(a, b, k));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+         
+       int n1 = a.length;
+       int n2 = b.length;
+       int[] l = new int[n1+n2];
+       int i=0,j=0,p=0;
+       while(i<n1){
+           l[p++] = a[i++];
+       }
+       while(j<n2){
+           l[p++] = b[j++];
+       }
+       Arrays.sort(l);
+       
+       return l[k-1];
+    }
+}
